9ae97634c209a124ce7db3042900b10fa6be12fe
Gautam Bajaj <gautam1237@gmail.com>
2015-10-22 18:34:02 +0900

    Removed information

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -8,7 +8,7 @@ class PostsControllerTest < ActionController::TestCase
 
   test "should fetch facebook post" do
   	sign_in User.find_by_email('humansofnewyork@facebook.com')
-  	get(:save_posts_from_facebook_page,{'id'=>'humansofnewyork'},{'access_token'=>'CAACEdEose0cBAGt7qJuVWnZCOMpIuKgW41mg7QSxCraZBZBkLxGZCv3jAYkZBCB7tZCV9e2NZBt7IEzENVdlxXuMJZB6IZC5dOuoMKSqGcfDv1n4JtzUyimtmLJATau28gEEq0utAtHB2JYaLwuZCwhWyPIawG0QsOTdyYprYQByvnFK5bLx3EyHU2trJMQdzh9PIKZBH8j6I4CqzPyh0r5LE0GjH4WQultTkoZD'})
+  	get(:save_posts_from_facebook_page,{'id'=>'humansofnewyork'},{'access_token'=>'asdknasd'})
   	assert_response :success
   end
 end
